3b95e628b3f4a687b47ca1a408674bec6656f599
Shosan-Code-Hub <shosanacodemia@gmail.com>
2024-10-24 08:54:33 +0100

    project init

---
+++ name.js
@@ -0,0 +1,6 @@
+let personalName = "Damilola Express";
+let x = 10;
+let y = 20;
+
+console.log("My name is " + personalName, "I am " + (x + y) + " years old.");
+
---
+++ person.js
@@ -0,0 +1,7 @@
+let person = {
+    name: 'John',
+    age: 30,
+    sayHello: function() {
+        console.log('Hello, my name is ' + this.name);
+    }
+};
